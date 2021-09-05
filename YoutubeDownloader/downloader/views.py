0c83a5ceb749cb82505f27dce9cdbc6fae03c02f
EllisK-L <ellis.keener.lacroix@gmail.com>
2021-09-04 23:49:38 -0300

    IDK what I added from the last commit

--- YoutubeDownloader/downloader/views.py
+++ YoutubeDownloader/downloader/views.py
@@ -12,13 +12,48 @@ import random
 # Create your views here.
 
 def downloading(request):
+    filename = ""
     if request.is_ajax and request.method == "POST":
         form = DownloadStartForm(request.POST)
         if form.is_valid():
-            ser_instance = serializers.serialize('json', [ instance, ])
+            error = ""
+            url = request.POST["link"]
+            ydl_opts = {
+                    "format":"bestaudio/best",
+                    'noplaylist' : True,
+                    "postprocessors":[{
+                        "key":"FFmpegExtractAudio",
+                        "preferredcodec":"mp3",
+                        "preferredquality":"192",
+                    }],
+                }
+            if url != "":
+                try:
+                    print("downloading "+url)
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        ydl.download([url])
+                    print("Finished Downloading")
+                    print("Moving")
+                    fileName = "song"+str(random.randint(0,100000))+".mp3"
+                    for file in os.listdir("./"):
+                        if file.endswith(".mp3"):
+                            os.rename(file,fileName)
+                            shutil.move(fileName,"media/downloader/")
+                            filename = "/media/downloader/" + fileName
+                except:
+                    error = "There was a problem, sorry :/"
+            else:
+                error = "Did you really try and download nothing -_-"
+
+            # ser_instance = serializers.serialize('json', [ request.POST, ])
             # send to client side.
-            return JsonResponse({"instance": ser_instance}, status=200)
-    return JsonResponse({"error": ""}, status=400)
+            if(error != ""):
+                return JsonResponse({"error": error}, status=400)
+    return JsonResponse({"instance": filename,"error":error}, status=200)
+
+
+
+
 
 def downloader(request):
     form = DownloadStartForm()
@@ -28,29 +63,5 @@ def downloader(request):
     "downloadLink":"none",
     "error":"none"
 }
-    if request.POST:
-        ydl_opts = {
-            "format":"bestaudio/best",
-            'noplaylist' : True,
-            "postprocessors":[{
-                "key":"FFmpegExtractAudio",
-                "preferredcodec":"mp3",
-                "preferredquality":"192",
-            }],
-        }
-        try:
-            if request.POST["link"] != "":
-                print("downloading"+request.POST["link"])
-                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                    ydl.download([request.POST["link"]])
-                print("Finished Downloading")
-                print("Moving")
-                fileName = "song"+str(random.randint(0,100000))+".mp3"
-                for file in os.listdir("./"):
-                    if file.endswith(".mp3"):
-                        os.rename(file,fileName)
-                        shutil.move(fileName,"static/downloader/")
-                        context["downloadLink"] = "/static/downloader/" + fileName
-        except:
-            context["error"] = "Yeah, I programmed this poorly, please try again or another link :)"
+        
     return render(request, "downloader/download_start.html",context=context)
